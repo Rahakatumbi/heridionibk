ab2e90caea8fb65934e5f42eb6d02e7dc6cdadee
Raha Faradja <35454137+Rahakatumbi@users.noreply.github.com>
2022-11-01 22:33:11 +0200

    Update db 2.go

--- config/db 2.go
+++ config/db 2.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+	"os"
 	"github.com/Raha2071/heridionibd/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -9,14 +11,17 @@ import (
 var DB *gorm.DB
 
 func SetupDB() {
-	// DB_USER := os.Getenv("DB_USER")
-	// DB_PASS := os.Getenv("DB_PASSWORD")
-	// DB_HOST := os.Getenv("DB_HOST")
-	// DB_DBNAME := os.Getenv("DB_DATABASE")
+	USER := os.Getenv("DB_USER")
+	PASS := os.Getenv("DB_PASSWORD")
+	HOST := os.Getenv("DB_HOST")
+	PORT := os.Getenv("DB_PORT")
+	DBNAME := os.Getenv("DB_DATABASE")
 
-	// URL := fmt.Sprintf("%s:%s@tcp(%s)/%s?
-	dsn := "root:admin@tcp(localhost:3306)/coffeedb?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	url := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)
+
+	db, err := gorm.Open(mysql.Open(url), &gorm.Config{})
+	// dsn := "root:@tcp(127.0.0.1:3306)/compusdb?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err.Error())
 	}
